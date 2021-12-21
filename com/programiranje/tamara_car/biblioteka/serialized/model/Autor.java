8ae54817aea06615a7322015cb3027cd762cf31d
tamara <tamara@liberanimus>
2021-12-21 19:32:35 +0100

    added Autor

---
+++ com/programiranje/tamara_car/biblioteka/serialized/model/Autor.java
@@ -0,0 +1,17 @@
+package programiranje.tamara_car.biblioteka.serialized.model;
+
+public class Autor extends Osoba {
+	
+	public Autor(int id, String ime, String prezime) {
+		super(id, ime, prezime);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Autor:  "+super.toString();
+	}
+	
+	
+	
+}
