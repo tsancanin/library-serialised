bd404da8e91bbefbdb7a2ae05fa51d222b7d0b96
tamara <tamara@liberanimus>
2021-12-22 14:43:11 +0100

    added Zanrovi

--- com/programiranje/tamara_car/biblioteka/serialized/model/Biblioteka.java
+++ com/programiranje/tamara_car/biblioteka/serialized/model/Biblioteka.java
@@ -1,6 +1,8 @@
 package programiranje.tamara_car.biblioteka.serialized.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Biblioteka implements Serializable{
 	/**
@@ -10,6 +12,13 @@ public class Biblioteka implements Serializable{
 	
 	private String naziv;
 	private String adresa;
+	
+	private ArrayList<Autor> autori = new ArrayList<Autor>();
+	private ArrayList<Clan> clanovi = new ArrayList<Clan>();
+	private ArrayList<EvidencijaKnjiga> evidencijaKnjiga = new ArrayList<EvidencijaKnjiga>();
+	private HashMap<Zanrovi, HashMap<Autor, ArrayList<Knjiga>>> sveKnjige = 
+			new HashMap<Zanrovi, HashMap<Autor,ArrayList<Knjiga>>>();
+	
 	
 	private Biblioteka() {}
 
@@ -23,6 +32,47 @@ public class Biblioteka implements Serializable{
 
 	public String getAdresa() {
 		return adresa;
+	}
+
+	public ArrayList<Autor> getAutori() {
+		return autori;
+	}
+
+	public void setAutori(ArrayList<Autor> autori) {
+		this.autori = autori;
+	}
+
+	public ArrayList<Clan> getClanovi() {
+		return clanovi;
+	}
+
+	public void setClanovi(ArrayList<Clan> clanovi) {
+		this.clanovi = clanovi;
+	}
+
+	public ArrayList<EvidencijaKnjiga> getEvidencijaKnjiga() {
+		return evidencijaKnjiga;
+	}
+
+	public void setEvidencijaKnjiga(ArrayList<EvidencijaKnjiga> evidencijaKnjiga) {
+		this.evidencijaKnjiga = evidencijaKnjiga;
+	}
+
+	public HashMap<Zanrovi, HashMap<Autor, ArrayList<Knjiga>>> getSveKnjige() {
+		return sveKnjige;
+	}
+
+	public void setSveKnjige(HashMap<Zanrovi, HashMap<Autor, ArrayList<Knjiga>>> sveKnjige) {
+		this.sveKnjige = sveKnjige;
+	}
+
+	public Biblioteka(ArrayList<Autor> autori, ArrayList<Clan> clanovi, ArrayList<EvidencijaKnjiga> evidencijaKnjiga,
+			HashMap<Zanrovi, HashMap<Autor, ArrayList<Knjiga>>> sveKnjige) {
+		super();
+		this.autori = autori;
+		this.clanovi = clanovi;
+		this.evidencijaKnjiga = evidencijaKnjiga;
+		this.sveKnjige = sveKnjige;
 	}
 
 	public void setAdresa(String adresa) {
---
+++ com/programiranje/tamara_car/biblioteka/serialized/model/Zanrovi.java
@@ -0,0 +1,5 @@
+package programiranje.tamara_car.biblioteka.serialized.model;
+
+public enum Zanrovi {
+	DRAMA,TRILER,HOROR,KOMEDIJA,FANTASTIKA
+}
